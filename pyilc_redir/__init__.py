8a8d058eec6b7055c583305ad4776403fb09747d
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-01-18 22:14:34 -0600

    removed junk

--- pyilc_redir/__init__.py
+++
@@ -1,6 +0,0 @@
-# import sys
-# sys.path.append('/home/jim/VSCode_Projects_SPACE/pyilc/pyilc')
-
-# # Angry yellow squiggles are expected under the import statements in the following lines.
-# from input import ILCInfo
-# from wavelets import Wavelets, wavelet_ILC, harmonic_ILC
